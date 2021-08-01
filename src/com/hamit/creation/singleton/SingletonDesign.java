8fe2f069a084c345f40e48f3be05f0709267be0b
HamitMizrak <hamitmizrak@gmail.com>
2021-08-01 13:46:29 +0300

    Singleton Main Test (psvm)

--- src/com/hamit/creation/singleton/SingletonDesign.java
+++ src/com/hamit/creation/singleton/SingletonDesign.java
@@ -1,16 +1,18 @@
 package com.hamit.creation.singleton;
 
 public class SingletonDesign {
+	// private
+	// private static
+	// public static
 
 	// 1- constructor ==> private
 	private SingletonDesign() {
-
 	}
 
-	// 2- nesneyi
+	// 2- instance
 	private static SingletonDesign instance;
 
-	// 3-
+	// 3- getInstance()
 	public static SingletonDesign getInstance() {
 		if (instance == null) {
 			synchronized (SingletonDesign.class) {
---
+++ src/com/hamit/creation/singleton/SingletonMainTest.java
@@ -0,0 +1,11 @@
+package com.hamit.creation.singleton;
+
+public class SingletonMainTest {
+
+	public static void main(String[] args) {
+
+		SingletonDesign.getInstance();
+
+	}
+
+}
